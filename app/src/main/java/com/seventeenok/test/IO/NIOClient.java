1f30ccb193eed8f4be05f0dc77137d21bc247075
androidlz <android_lz@163.com>
2019-06-19 14:11:08 +0800

    哈哈哈哈

--- app/src/main/java/com/seventeenok/test/IO/NIOClient.java
+++ app/src/main/java/com/seventeenok/test/IO/NIOClient.java
@@ -2,6 +2,7 @@ package com.seventeenok.test.IO;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.SocketChannel;
@@ -13,40 +14,49 @@ public class NIOClient {
 
     /**
      * 获得一个连接Socket通道，并对通道进行初始化工作
+     *
      * @param ip
      * @param port
      */
-    public void initClient(String ip,int port) throws IOException {
+    public void initClient(String ip, int port) throws IOException {
         //获得一个Socket通道
-        SocketChannel channel= SocketChannel.open();
+        SocketChannel channel = SocketChannel.open();
         //设置通道为非阻塞
         channel.configureBlocking(false);
         //获得一个通道管理器
         selector = Selector.open();
         channel.finishConnect();
         //客户端连接服务器，其实方法并没有实现连接，需要在listen()方法中调用channel.finishConnect()才能完成连接
-        channel.connect(new InetSocketAddress(ip,port));
+        channel.connect(new InetSocketAddress(ip, port));
         //将通道管理器和该通道绑定，并为该通道注册SelectionKey.OP_CONNECT事件
-        channel.register(selector,SelectionKey.OP_CONNECT);
+        channel.register(selector, SelectionKey.OP_CONNECT);
     }
 
     /**
      * 采用轮询的方式监听selector上是否有需要处理的事件，如有，则进行处理。
      */
     public void listen() throws IOException {
-        while (true){
+        while (true) {
             selector.select();
             //获得selector中选中项的迭代器
             Iterator<SelectionKey> iterator = selector.selectedKeys().iterator();
-            while(iterator.hasNext()){
+            while (iterator.hasNext()) {
                 SelectionKey key = iterator.next();
                 //删除已选中的key,以防重复处理
-//奥术大师多
-                //asda
+                iterator.remove();
+                if (key.isConnectable()) {
+                    SocketChannel channel = (SocketChannel) key.channel();
+                    //如果正在连接 ，则连接完成
+                    if (channel.isConnectionPending()) {
+                        channel.isConnected();
+                    }
+                    //设置成非阻塞的
+                    channel.configureBlocking(false);
+                    channel.write(ByteBuffer.wrap(new String("向服务端发送一条消息").getBytes()));
+                    //在和服务端连接成功之后，为了可以接收到服务端的信息，需要给通道加读的权限。
+                    channel.register(selector, SelectionKey.OP_READ);
+                }
             }
-
-
-
         }
     }
 }
--- app/src/main/java/com/seventeenok/test/IO/NIOServer.java
+++ app/src/main/java/com/seventeenok/test/IO/NIOServer.java
@@ -1,4 +1,69 @@
 package com.seventeenok.test.IO;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+
 public class NIOServer {
+
+    private Selector selector;
+
+    public void initServer(int port) throws IOException {
+        ServerSocketChannel socketChannel = ServerSocketChannel.open();
+        //设置为非阻塞
+        socketChannel.configureBlocking(false);
+        //将该通道对应的ServerSocket绑定到port端口
+        socketChannel.socket().bind(new InetSocketAddress(port));
+        //获得一个通道管理器
+        selector = Selector.open();
+        //将通道管理器和通道绑定，并为该通道注册SelectionKey.OP_ACCEPT事件，注册该事件后，
+        //当该事件到达时，selector.select（）会返回，如果没到达，Selector.select()会一直阻塞
+        socketChannel.register(selector, SelectionKey.OP_ACCEPT);
+    }
+
+    /**
+     * 采用轮询的方式监听selector上是否有需要处理的事件，如有，则进行处理。
+     *
+     * @throws IOException
+     */
+    public void listenen() throws IOException {
+        while (true) {
+            selector.select();
+            Iterator<SelectionKey> iterator = selector.selectedKeys().iterator();
+            if (iterator.hasNext()) {
+                SelectionKey next = iterator.next();
+                iterator.remove();
+                if (next.isAcceptable()) {
+                    ServerSocketChannel server = (ServerSocketChannel) next.channel();
+                    SocketChannel socketChannel = server.accept();
+                    socketChannel.configureBlocking(false);
+                    //向客户端发送消息
+                    socketChannel.write(ByteBuffer.wrap(new String("向客户端发送消息").getBytes()));
+                    //在和客户端连接成功之后，为了可以接收到客户端你的信息，给通道设置读权限
+                    socketChannel.register(selector,SelectionKey.OP_READ);
+
+                    //获得了可读事件
+                }else if(next.isReadable()){
+                    read(next);
+                }
+            }
+        }
+    }
+
+    /**
+     * 处理客户端发送过来信息的事件
+     * @param next
+     * @throws IOException
+     */
+    private void read(SelectionKey next) throws IOException{
+        //服务器可读消息：得到事件发生的Socket通道
+        SocketChannel channel = (SocketChannel) next.channel();
+
+    }
+
 }
